eae5f569fb96596ba97f93ead239cea77ce99450
OlgaHook <olga_krjuckova@inbox.lv>
2021-07-30 13:40:16 +0300

    ViewController

---
+++ CalendarDayFinder/Controller/AppearanceViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AppearanceViewController.swift
+//  CalendarDayFinder
+//
+//  Created by olga.krjuckova on 30/07/2021.
+//
+
+import UIKit
+
+class AppearanceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func closeButtenTapped(_ sender: Any) {
+        self.dismiss(animated: true
+        , completion: nil)
+    }
+    
+
+}
